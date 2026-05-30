245416ebf20b650b62b40842bcb49c3482a39c8d
xjeddah <55954113+xjeddah@users.noreply.github.com>
2026-05-31 00:44:51 +0300

    fix: remove e2e fixture target/ from git, add .gitignore

--- tests/e2e_governed_task/target/debug/deps/fixture_lib-f24288bfa1f6b8df.d
+++
@@ -1,5 +0,0 @@
-C:\Next-Era\OpenWand\tests\e2e_governed_task\target\debug\deps\fixture_lib-f24288bfa1f6b8df.d: fixture_lib\src\lib.rs
-
-C:\Next-Era\OpenWand\tests\e2e_governed_task\target\debug\deps\fixture_lib-f24288bfa1f6b8df.exe: fixture_lib\src\lib.rs
-
-fixture_lib\src\lib.rs:
